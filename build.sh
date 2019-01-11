c4856ca522c04dc217e869eb0f2175b83211efd7
Alexander Grund <alexander.grund@tu-dresden.de>
2019-01-11 14:21:20 +0100

    Improve comment to Unix make

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ mkdir -p build && cd build
 
 # Configure
 cmake -DCODE_COVERAGE=ON -DCMAKE_BUILD_TYPE=Debug ..
-# Build (on Unix also: `make -j $(nproc)`)
+# Build (for Make on Unix equivalent to `make -j $(nproc)`)
 cmake --build . --config Debug -- -j $(nproc)
 # Test
 ctest -j $(nproc) --output-on-failure
